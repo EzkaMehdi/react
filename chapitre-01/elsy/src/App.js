caa9498a831c62c88181ca8b939b6c531cf69c0f
Mehdi <ezka.mehdi@gmail.com>
2021-04-09 17:50:16 +0200

    pt 5

--- chapitre-01/elsy/src/App.js
+++ chapitre-01/elsy/src/App.js
@@ -1,24 +1,27 @@
+
 import React from "react";
 import './App.css';
-import Steps from "./components/steps.jsx"
-import '../src/styles/global.css';
-
-
-const tempMin = -20
-const tempMax = 40
-const heartMin = 80
-const heartMax = 180
-const stepsMin = 0
-const stepsMax = 50000
-
-
-
+import Box from './components/Box.jsx'
+import './styles/global.css'
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 class App extends React.Component {
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row"> <Steps />
-          
+      <div className="container-fluid">
+        <div className="row">
+          {/* Water */}
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" ></Box >
+          {/* Steps */}
+          <Box icon="directions_walk" color="black" value={3000} unit="steps" ></Box >
+          {/* Heart */}
+          <Box icon="favorite" color="red" value={120} unit="bpm" ></Box >
+          {/* Température */}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" ></Box >
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
@@ -27,5 +30,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
