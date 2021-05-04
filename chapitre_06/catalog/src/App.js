f21421ad3039cef331cb4e4ed03f3b6afd884291
Mehdi <ezka.mehdi@gmail.com>
2021-05-04 17:28:19 +0200

    catalog

---
+++ chapitre_06/catalog/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/catalog">Catalog</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/catalog">
+            <Catalog />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+
+
+function Home() {
+  return (
+    <ul>
+      <li>A Wrinkle in Time</li>
+      <li>The Strangers: Prey at Night</li>
+      <li>The Hurricane Heist</li>
+      <li>Gringo</li>
+    </ul>
+  )
+}
+
+function Catalog() {
+  return <h2>Catalog</h2>;
+}export default function App() 
