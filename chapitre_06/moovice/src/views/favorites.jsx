8f2f8d287f94f43647807f6b6b64f8d9b2ab9224
Mehdi <ezka.mehdi@gmail.com>
2021-05-17 12:31:38 +0200

    moovice

--- chapitre_06/moovice/src/views/favorites.jsx
+++ chapitre_06/moovice/src/views/favorites.jsx
@@ -2,23 +2,24 @@ import React from "react";
 
 
 class Favorites extends React.Component {
-getStorage(){
-  return(
-    movieID
-  )
-}
+
   constructor() {
     super();
 
     this.state = {  
       movies: [],
-      favIDs:this.getStorage
+      favIDs:localStorage.getItem("movies")
     };
   }
   getMovie(id){
+    const API_KEY = 'e441f8a3a151d588a4932d2c5d310769'
     const url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e441f8a3a151d588a4932d2c5d310769"
   }
 
+  componentDidMount(){
+
+    
+  }
   render() {
     return (
       <div>
